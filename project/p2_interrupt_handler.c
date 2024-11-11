23e9bfdaabc302966b8980d7bf0feb3ad93576e1
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-11 01:02:27 -0700

    Update p2_interrupt_handler.c

--- project/p2_interrupt_handler.c
+++ project/p2_interrupt_handler.c
@@ -3,7 +3,7 @@
 
 /* Switch on P1 (S2) */
 void
-__interrupt_vec(PORT1_VECTOR) Port_1(){
+__interrupt_vec(PORT1_VECTOR) Port_2(){
   if (P1IFG & SWITCHES) {	      /* did a button cause this interrupt? */
     P1IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
     switch_interrupt_handler();	/* single handler for all switches */
