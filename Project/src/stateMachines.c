d4c38bd7377fb7905b90c39054c1b1fe728dff76
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 10:21:19 -0700

    Update stateMachines.c

--- Project/src/stateMachines.c
+++ Project/src/stateMachines.c
@@ -3,36 +3,29 @@
 #include "led.h"
 #include "switches.h"
 
-// Declare external variables
-extern unsigned char led_changed, green_on, red_on, led_state;
-
 static enum {off=0, dim=1, bright=2} ledMode;
 static char pwmCount = 0;
 
-void sm_slow_clock()	/* slowly cycle through {off, dim, bright} */
+void sm_slow_clock()  /* Slowly cycle through {off, dim, bright} */
 {
   ledMode = (ledMode + 1) % 3;
 }
 
-void sm_fast_clock()	/* quickly cycle through 0...3 */
+void sm_fast_clock()  /* Quickly cycle through 0...3 */
 {
-  pwmCount = (pwmCount + 1) & 3;
+  pwmCount = (pwmCount + 1) & 3; 
 }
 
 void sm_update_led()
 {
   char new_red_on;
-  switch (ledMode)
-  {
+  switch (ledMode){
   case off:
-    new_red_on = 0;
-    break;
+    new_red_on = 0; break;
   case bright:
-    new_red_on = 1;
-    break;
+    new_red_on = 1; break;
   case dim:
-    new_red_on = (pwmCount < 1); // 25% duty cycle
-    break;
+    new_red_on = (pwmCount < 1); break; /* 25% duty cycle */
   }
   if (red_on != new_red_on)
   {
@@ -41,12 +34,10 @@ void sm_update_led()
   }
 }
 
-char toggle_red()	/* always toggle! */
+char toggle_red()  /* Always toggle! */
 {
   static char state = 0;
-
-  switch (state)
-  {
+  switch (state) {
   case 0:
     red_on = 1;
     state = 1;
@@ -56,37 +47,29 @@ char toggle_red()	/* always toggle! */
     state = 0;
     break;
   }
-  return 1; // always changes an LED
+  return 1;  /* Always changes an LED */
 }
 
-char toggle_green()	/* only toggle green if red is on!  */
+char toggle_green()  /* Only toggle green if red is on! */
 {
   char changed = 0;
-  if (red_on)
-  {
+  if (red_on) {
     green_on ^= 1;
     changed = 1;
   }
   return changed;
 }
 
-void state_advance()		/* alternate between toggling red & green */
+void state_advance()  /* Alternate between toggling red & green */
 {
   char changed = 0;
 
   static enum {R=0, G=1} color = G;
-  switch (color)
-  {
-  case R:
-    changed = toggle_red();
-    color = G;
-    break;
-  case G:
-    changed = toggle_green();
-    color = R;
-    break;
+  switch (color) {
+  case R: changed = toggle_red(); color = G; break;
+  case G: changed = toggle_green(); color = R; break;
   }
 
-  led_changed = changed; // switch_state_changed
+  led_changed = changed;  // Switch state changed
   led_update();
 }
