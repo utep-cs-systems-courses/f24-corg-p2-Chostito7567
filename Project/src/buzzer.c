3ed3711d3b6bf2a820994f62b87287d4c9722e38
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 09:45:51 -0700

    Update buzzer.c

--- Project/src/buzzer.c
+++ Project/src/buzzer.c
@@ -2,7 +2,7 @@
 #include "libTimer.h"
 #include "buzzer.h"
 
-#define CLOCK_FREQUENCY 1000000  // Assuming 1 MHz clock frequency
+#define CLOCK_FREQUENCY 1000000  // Assuming 1 MHz clock frequency for your MSP430
 
 // Define different jingles (frequency in Hz and duration in ms)
 int jingle1[][2] = {{1000, 200}, {1200, 200}, {1400, 200}, {0, 0}};
@@ -12,11 +12,11 @@ int jingle4[][2] = {{2500, 400}, {2300, 400}, {2100, 400}, {0, 0}};
 
 // Initialize the buzzer
 void buzzer_init() {
-    timerAUpmode();		    /* used to drive speaker */
+    timerAUpmode();  /* used to drive speaker */
     P2SEL2 &= ~(BIT6 | BIT7);
     P2SEL &= ~BIT7; 
     P2SEL |= BIT6;
-    P2DIR = BIT6;		      /* enable output to speaker (P2.6) */
+    P2DIR = BIT6;  /* enable output to speaker (P2.6) */
 }
 
 // Set buzzer frequency
@@ -25,14 +25,26 @@ void buzzer_set_period(short cycles) {
     CCR1 = cycles >> 1;  // one half cycle
 }
 
+// Custom delay function based on loop iteration
+void delay_ms(unsigned int ms) {
+    volatile unsigned int i, j;
+    for (i = 0; i < ms; i++) {
+        for (j = 0; j < 1000; j++) {
+            // Empty loop to create a delay
+            // This will give a rough delay depending on clock speed
+        }
+    }
+}
+
 // Plays a jingle based on the input array (frequency and duration pairs)
 void play_jingle(int jingle[][2]) {
     for (int i = 0; jingle[i][0] != 0; i++) {
-        buzzer_set_period(1000000 / jingle[i][0]); // Set frequency
-        unsigned int delay_cycles = (CLOCK_FREQUENCY / 1000) * jingle[i][1]; // Calculate delay in cycles
-        __delay_cycles(delay_cycles); // Delay in cycles for milliseconds
+        buzzer_set_period(1000000 / jingle[i][0]);  // Set frequency
+        
+        // Delay for the given duration (in ms)
+        delay_ms(jingle[i][1]);  // Delay in milliseconds
     }
-    buzzer_set_period(0); // Turn off the buzzer after playing
+    buzzer_set_period(0);  // Turn off the buzzer after playing
 }
 
 // Existing tone-shifting sound function
@@ -44,7 +56,7 @@ void buzzer_play_sound() {
     // Positive Change Rate
     if (change_rate > 0) {
         if (song_length > 90000) {
-            change_rate = -change_rate; // Reverse the change
+            change_rate = -change_rate;  // Reverse the change
             song_length = song_length + (change_rate << 1);
         }
     }
@@ -52,7 +64,7 @@ void buzzer_play_sound() {
     // Negative Change Rate
     if (change_rate < 0) {
         if (song_length < 900) {
-            change_rate = -change_rate; // Reverse the change
+            change_rate = -change_rate;  // Reverse the change
             song_length = song_length + (change_rate << 1);
         }
     }
