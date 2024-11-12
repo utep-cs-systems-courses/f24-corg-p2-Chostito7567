2a20e9edb436cfcaa907545e380eba5b3676ccf8
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 13:00:51 -0700

    Update switches.c

--- Project/src/switches.c
+++ Project/src/switches.c
@@ -1,81 +1,41 @@
 #include <msp430.h>
 #include "switches.h"
 #include "led.h"
-#include "stateMachines.h"
 #include "buzzer.h"
 
-short sound;
-char switch_state_down, period, switch_state_changed; /* Effectively boolean */
+char switch_state_down, switch_state_changed;
 
-static char 
-switch_update_interrupt_sense()
-{
-  char p2val = P2IN;
-  P2IES |= (p2val & SWITCHES);    /* If switch up, sense down */
-  P2IES &= (p2val | ~SWITCHES);   /* If switch down, sense up */
-  return p2val;
+static char switch_update_interrupt_sense() {
+    char p2val = P2IN;
+    P2IES |= (p2val & SWITCHES);   // Sense high-to-low
+    P2IES &= (p2val | ~SWITCHES);  // Sense low-to-high
+    return p2val;
 }
 
-void switch_init()
-{  
-  P2REN |= SWITCHES;    /* Enables resistors for switches */
-  P2IE = SWITCHES;      /* Enable interrupts from switches */
-  P2OUT |= SWITCHES;    /* Pull-ups for switches */
-  P2DIR &= ~SWITCHES;   /* Set switches' bits for input */
-  switch_update_interrupt_sense();
-  switch_interrupt_handler();
-  led_update();
+void switch_init() {
+    P2REN |= SWITCHES;  // Enable resistors for switches
+    P2IE |= SWITCHES;   // Enable interrupts from switches
+    P2OUT |= SWITCHES;  // Pull-ups for switches
+    P2DIR &= ~SWITCHES; // Set switches' bits for input
+    switch_update_interrupt_sense();
 }
 
-void switch_interrupt_handler()
-{
-  char p2val = switch_update_interrupt_sense();
-  char sw_button_1 = (p2val & SW1) ? 0 : SW1;
-  char sw_button_2 = (p2val & SW2) ? 0 : SW2;
-  char sw_button_3 = (p2val & SW3) ? 0 : SW3;
-  char sw_button_4 = (p2val & SW4) ? 0 : SW4;
+void switch_interrupt_handler() {
+    char p2val = switch_update_interrupt_sense();
 
-  if(sw_button_1){
-    led_state = 0;
-    sound = 1000;
-    period = 10;
-    buzzer_play_sound();  // Play sound for SW1
+    if (!(p2val & SW1)) {
+        play_jingle1();
+        led_state = 0;
+    } else if (!(p2val & SW2)) {
+        play_jingle2();
+        led_state = 1;
+    } else if (!(p2val & SW3)) {
+        play_jingle3();
+        led_state = 2;
+    } else if (!(p2val & SW4)) {
+        play_jingle4();
+        led_state = 3;
+    }
     led_changed = 1;
-    led_advance();
     led_update();
-    switch_state_down = 1;
-  }
-
-  if(sw_button_2){
-    led_state = 0;
-    sound = 2000;
-    period = 20;
-    buzzer_play_sound();  // Play sound for SW2
-    led_changed = 1;
-    led_advance();
-    led_update();
-    switch_state_down = 1;
-  }
-
-  if(sw_button_3){
-    led_state = 0;
-    sound = 3000;
-    period = 30;
-    buzzer_play_sound();  // Play sound for SW3
-    led_changed = 1;
-    led_advance();
-    led_update();
-    switch_state_down = 1;
-  }
-
-  if(sw_button_4){
-    led_state = 0;
-    sound = 4000;
-    period = 40;
-    buzzer_play_sound();  // Play sound for SW4
-    led_changed = 1;
-    led_advance();
-    led_update();
-    switch_state_down = 1;
-  }
 }
