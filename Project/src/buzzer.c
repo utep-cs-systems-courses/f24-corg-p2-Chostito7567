811aafe6b48d864bb5fb9f4516e5f3eb67e64c3d
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 09:30:10 -0700

    Update buzzer.c

--- Project/src/buzzer.c
+++ Project/src/buzzer.c
@@ -38,7 +38,7 @@ void buzzer_set_period(short cycles) {
 void play_jingle(int jingle[][2]) {
     for (int i = 0; jingle[i][0] != 0; i++) {
         buzzer_set_period(1000000 / jingle[i][0]); // Set frequency
-        __delay_cycles(1000 * jingle[i][1]);      // Delay in ms
+        delay_cycles(1000 * jingle[i][1]);      // Delay in ms
     }
     buzzer_set_period(0); // Turn off the buzzer after playing
 }
