0936b35303f34007f52627376c6c5b577e599102
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 09:49:30 -0700

    Update stateMachines.h

--- Project/src/stateMachines.h
+++ Project/src/stateMachines.h
@@ -1,12 +1,13 @@
-
-
 #ifndef stateMachine_included
 #define stateMachine_included
 
+// Function prototypes for handling LED updates and button presses
 void state_advance();
 char toggle_red();
 char toggle_green();
 char toggle_red_green();
 
+// New function to handle button presses and play jingles
+void handle_button_press(char button);
 
-#endif // included
+#endif // stateMachine_included
