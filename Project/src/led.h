f0d0a9a1c633ddaf2d53fd23494cd31943c16096
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 10:00:06 -0700

    Update led.h

--- Project/src/led.h
+++ Project/src/led.h
@@ -5,13 +5,11 @@
 #define LED_GREEN BIT6             // P1.6
 #define LEDS (BIT0 | BIT6)
 
-void led_init();
-void led_update();
-void led_pattern1();
-void led_pattern2();
-void led_pattern3();
-void led_pattern4();
+void led_init();		/* initialize LEDs */
+void led_update();		/* update leds */
 
-extern unsigned char led_changed, green_led_state, red_led_state, green_on, red_on, led_state;
+extern unsigned char led_changed, green_on, red_on, led_state;
+void led_toggle();
+void led_advance();
 
 #endif
