a54c56def30a251e011c3600dbfb8b1c7c7d0013
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 12:47:16 -0700

    Update led.c

--- Project/src/led.c
+++ Project/src/led.c
@@ -1,46 +1,33 @@
 #include <msp430.h>
 #include "led.h"
-#include "switches.h"
-#include "buzzer.h"
+#include "stateMachines.h"
 
-unsigned char led_changed = 0, green_on = 0, red_on = 0, led_state = 0;
-static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
-char frequency;
-static char ledFlags = 0;
+// Definition of ledFlags in led.c
+static char ledFlags = 0; // Define and initialize ledFlags. `static` makes it private to this file.
 
-void led_init()
-{
-  P1DIR |= LEDS;                    // Set bits for LEDs as output
+unsigned char led_changed = 0, green_on=0, red_on=0, led_state=0;
+
+void led_init() {
+  P1DIR |= LEDS;  // Set LED pins as output
   led_changed = 1;
   led_update();
 }
 
-void led_update()
-{
-  if (led_changed)
-  {
-    ledFlags = 0;
-    if (!(led_state)){
+void led_update() {
+  if (led_changed) {
+    if (!(led_state)) {
       ledFlags |= LED_RED;
-    }
-    else {
+    } else {
       ledFlags |= LED_GREEN;
     }
-
-    P1OUT &= (0xff^LEDS) | ledFlags; // Clear bits for off LEDs
-    P1OUT |= ledFlags;               // Set bits for on LEDs
+    
+    P1OUT &= (0xff ^ LEDS) | ledFlags; // Clear bits for off LEDs
+    P1OUT |= ledFlags; // Set bits for on LEDs
     led_changed = 0;
   }
 }
 
-void led_toggle()
-{
-  led_state = 1 - led_state;
-  led_changed = 1;
-}
-
-void led_advance()
-{
+void led_advance() {
   led_state += 1;
   led_changed = 1;
 }
