83601a23a89ddd13b015a96f34516509486f835f
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 10:16:53 -0700

    Update switches.c

--- Project/src/switches.c
+++ Project/src/switches.c
@@ -5,30 +5,28 @@
 #include "buzzer.h"
 
 short sound;
-char switch_state_down, period, switch_state_changed; /* effectively boolean */
+char switch_state_down, period, switch_state_changed; /* Effectively boolean */
 
 static char 
 switch_update_interrupt_sense()
 {
   char p2val = P2IN;
-  P2IES |= (p2val & SWITCHES);    /* if switch up, sense down */
-  P2IES &= (p2val | ~SWITCHES);    /* if switch down, sense up */
+  P2IES |= (p2val & SWITCHES);    /* If switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);   /* If switch down, sense up */
   return p2val;
 }
 
-// Initialize Switch Control
 void switch_init()
 {  
-  P2REN |= SWITCHES;    /* enables resistors for switches */
-  P2IE = SWITCHES;      /* enable interrupts from switches */
-  P2OUT |= SWITCHES;    /* pull-ups for switches */
-  P2DIR &= ~SWITCHES;   /* set switches' bits for input */
+  P2REN |= SWITCHES;    /* Enables resistors for switches */
+  P2IE = SWITCHES;      /* Enable interrupts from switches */
+  P2OUT |= SWITCHES;    /* Pull-ups for switches */
+  P2DIR &= ~SWITCHES;   /* Set switches' bits for input */
   switch_update_interrupt_sense();
   switch_interrupt_handler();
-  led_update();  // Update LEDs at startup
+  led_update();
 }
 
-// Change Button States
 void switch_interrupt_handler()
 {
   char p2val = switch_update_interrupt_sense();
@@ -37,52 +35,44 @@ void switch_interrupt_handler()
   char sw_button_3 = (p2val & SW3) ? 0 : SW3;
   char sw_button_4 = (p2val & SW4) ? 0 : SW4;
 
-  // Button 1: Jingle 1 (set frequency, change LEDs)
-  if(sw_button_1)
-  {
-    led_state = 0;  // Example LED pattern for button 1
+  if(sw_button_1){
+    led_state = 0;
     sound = 1000;
     period = 10;
-    buzzer_play_sound();
+    buzzer_play_sound();  // Play sound for SW1
     led_changed = 1;
     led_advance();
     led_update();
     switch_state_down = 1;
   }
 
-  // Button 2: Jingle 2
-  if(sw_button_2)
-  {
-    led_state = 1;  // Another LED pattern for button 2
+  if(sw_button_2){
+    led_state = 0;
     sound = 2000;
     period = 20;
-    buzzer_play_sound();
+    buzzer_play_sound();  // Play sound for SW2
     led_changed = 1;
     led_advance();
     led_update();
     switch_state_down = 1;
   }
 
-  // Button 3: Jingle 3
-  if(sw_button_3)
-  {
-    led_state = 2;  // Another LED pattern for button 3
+  if(sw_button_3){
+    led_state = 0;
     sound = 3000;
     period = 30;
-    buzzer_play_sound();
+    buzzer_play_sound();  // Play sound for SW3
     led_changed = 1;
     led_advance();
     led_update();
     switch_state_down = 1;
   }
 
-  // Button 4: Jingle 4
-  if(sw_button_4)
-  {
-    led_state = 3;  // Another LED pattern for button 4
+  if(sw_button_4){
+    led_state = 0;
     sound = 4000;
     period = 40;
-    buzzer_play_sound();
+    buzzer_play_sound();  // Play sound for SW4
     led_changed = 1;
     led_advance();
     led_update();
