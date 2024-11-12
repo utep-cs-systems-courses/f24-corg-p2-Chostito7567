ec6f8e1ce5eaccf3deebdce093cc870217d346e3
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 09:22:59 -0700

    Update led.c

--- Project/src/led.c
+++ Project/src/led.c
@@ -1,49 +1,47 @@
 #include <msp430.h>
 #include "led.h"
-#include "switches.h"
-#include "buzzer.h"
 
-unsigned char led_changed = 0, green_on = 0, red_on = 0, led_state = 0;
-
-// Initialize LEDs on the board
-void led_init()
-{
-  P1DIR |= LEDS;                      // Set LED pins as outputs
-  led_changed = 1;                    // Trigger an update to set initial LED state
-  led_update();
+void led_init() {
+    P1DIR |= LEDS;            // Set LED pins as outputs
+    led_update();
 }
 
-void led_update()
-{
-  if (led_changed)
-  {
-    // Turn off both LEDs initially
-    P1OUT &= ~LEDS; 
-
-    // Set the LED state based on led_state variable
-    switch (led_state)
-    {
-      case 0:
-        P1OUT |= LED_RED;  // Turn on RED LED
-        break;
-      case 1:
-        P1OUT |= LED_GREEN; // Turn on GREEN LED
-        break;
+void led_update() {
+    P1OUT &= ~LEDS; // Turn off both LEDs initially
+    if (led_state) {
+        P1OUT |= LED_GREEN;
+    } else {
+        P1OUT |= LED_RED;
     }
-    led_changed = 0; // Reset the flag after updating LEDs
-  }
 }
 
-// Toggle between Red and Green LED
-void led_toggle()
-{
-  led_state = 1 - led_state; // Toggle between 0 (Red) and 1 (Green)
-  led_changed = 1;           // Mark change for update
+void led_pattern1() {
+    for (int i = 0; i < 3; i++) {
+        P1OUT |= LED_RED;
+        __delay_cycles(500000);
+        P1OUT &= ~LED_RED;
+        __delay_cycles(500000);
+    }
 }
 
-// Advance the LED state in a cyclic manner
-void led_advance()
-{
-  led_state = (led_state + 1) % 2; // Cycle between 0 and 1
-  led_changed = 1;                 // Mark change for update
+void led_pattern2() {
+    for (int i = 0; i < 5; i++) {
+        P1OUT ^= LED_GREEN;
+        __delay_cycles(300000);
+    }
+}
+
+void led_pattern3() {
+    for (int i = 0; i < 2; i++) {
+        P1OUT |= LED_RED;
+        __delay_cycles(700000);
+        P1OUT &= ~LED_RED;
+    }
+}
+
+void led_pattern4() {
+    P1OUT |= LED_GREEN;
+    __delay_cycles(200000);
+    P1OUT &= ~LED_GREEN;
+    __delay_cycles(200000);
 }
