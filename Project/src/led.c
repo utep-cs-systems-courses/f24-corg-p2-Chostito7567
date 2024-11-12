8678b9567b3603d467f09ac8aa1202fa02b78db7
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 08:43:50 -0700

    Update led.c

--- Project/src/led.c
+++ Project/src/led.c
@@ -1,20 +1,15 @@
-
-
 #include <msp430.h>
 #include "led.h"
 #include "switches.h"
 #include "buzzer.h"
 
-unsigned char led_changed = 0, green_on=0, red_on=0, led_state=0;
-static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
-char freqeuency;
-static char ledFlags = 0;
+unsigned char led_changed = 0, green_on = 0, red_on = 0, led_state = 0;
 
-// Keep P1 because LEDs are on the board
+// Initialize LEDs on the board
 void led_init()
 {
-  P1DIR |= LEDS;		                      // bits attached to leds are output
-  led_changed = 1;
+  P1DIR |= LEDS;                      // Set LED pins as outputs
+  led_changed = 1;                    // Trigger an update to set initial LED state
   led_update();
 }
 
@@ -22,29 +17,33 @@ void led_update()
 {
   if (led_changed)
   {
-    ledFlags=0;
-    if(!(led_state)){
-      ledFlags |= LED_RED;
-    }
-    else
+    // Turn off both LEDs initially
+    P1OUT &= ~LEDS; 
+
+    // Set the LED state based on led_state variable
+    switch (led_state)
     {
-      ledFlags |= LED_GREEN;
+      case 0:
+        P1OUT |= LED_RED;  // Turn on RED LED
+        break;
+      case 1:
+        P1OUT |= LED_GREEN; // Turn on GREEN LED
+        break;
     }
-    
-    P1OUT &= (0xff^LEDS) | ledFlags; // clear bit for off leds
-    P1OUT |= ledFlags;		     // set bit for on leds
-    led_changed = 0;
+    led_changed = 0; // Reset the flag after updating LEDs
   }
 }
 
+// Toggle between Red and Green LED
 void led_toggle()
 {
-  led_state = 1 - led_state;
-  led_changed = 1;
+  led_state = 1 - led_state; // Toggle between 0 (Red) and 1 (Green)
+  led_changed = 1;           // Mark change for update
 }
 
+// Advance the LED state in a cyclic manner
 void led_advance()
 {
-  led_state+=1;
-  led_changed = 1;
+  led_state = (led_state + 1) % 2; // Cycle between 0 and 1
+  led_changed = 1;                 // Mark change for update
 }
