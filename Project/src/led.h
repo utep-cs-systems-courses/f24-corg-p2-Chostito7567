56b7c77bd3f89d120edc2c94055915cc7bc6d193
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 09:22:41 -0700

    Update led.h

--- Project/src/led.h
+++ Project/src/led.h
@@ -1,6 +1,3 @@
-
-#include <msp430.h>
-
 #ifndef led_included
 #define led_included
 
@@ -8,11 +5,13 @@
 #define LED_GREEN BIT6             // P1.6
 #define LEDS (BIT0 | BIT6)
 
-void led_init();       /* initialize LEDs */
-void led_update();     /* update leds */
-void led_toggle();     /* toggle between Red and Green */
-void led_advance();    /* advance led state */
+void led_init();
+void led_update();
+void led_pattern1();
+void led_pattern2();
+void led_pattern3();
+void led_pattern4();
 
-extern unsigned char led_changed, green_on, red_on, led_state;
+extern unsigned char led_changed, green_led_state, red_led_state, green_on, red_on, led_state;
 
 #endif
