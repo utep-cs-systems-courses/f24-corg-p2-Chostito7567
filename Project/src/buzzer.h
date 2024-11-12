ca98a14db8b8fb158a8a914da5f87c671dc7f845
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-11-12 10:15:41 -0700

    Update buzzer.h

--- Project/src/buzzer.h
+++ Project/src/buzzer.h
@@ -3,13 +3,6 @@
 
 void buzzer_init();
 void buzzer_set_period(short cycles);
-void buzzer_play_sound();
-void play_jingle(int jingle[][2]);
+void buzzer_play_sound();  // Declare the function here
 
-// Declare jingles as extern so they can be used in other files
-extern int jingle1[][2];
-extern int jingle2[][2];
-extern int jingle3[][2];
-extern int jingle4[][2];
-
-#endif
+#endif // buzzer_included
