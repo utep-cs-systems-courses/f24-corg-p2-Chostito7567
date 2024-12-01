a7035ca459c28136d4678a93d033fc055f877ebe
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-01 16:35:44 -0700

    Update lcdgame.h

--- Project/src/lcdgame.h
+++ Project/src/lcdgame.h
@@ -1,6 +1,9 @@
 #ifndef LCDGAME_H
 #define LCDGAME_H
 
+#include "../lcdLib/lcdutils.h"  // Correct path to lcdutils.h
+#include "../lcdLib/lcddraw.h"   // Correct path to lcddraw.h
+
 extern int lives;  // Declare `lives` as a global variable
 
 void lcd_game_init();
