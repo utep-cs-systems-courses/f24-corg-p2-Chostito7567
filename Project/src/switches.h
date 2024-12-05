5529d936866338bea7a935b5b6b7f9446597f17d
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-04 18:11:16 -0700

    Update switches.h

--- Project/src/switches.h
+++ Project/src/switches.h
@@ -1,11 +1,11 @@
 #ifndef switches_included
 #define switches_included
 
+#define SW1 BIT0       // Button 1
+#define SW2 BIT1       // Button 2
+#define SW3 BIT2       // Button 3
+#define SW4 BIT3       // Button 4
 #define SWITCHES (SW1 | SW2 | SW3 | SW4)
-#define SW1 BIT0
-#define SW2 BIT1
-#define SW3 BIT2
-#define SW4 BIT3
 
 void switch_init();
 void switch_interrupt_handler();
