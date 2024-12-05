4508c3132eeb5f8d99613239b2f24fe3a822f707
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-04 18:34:16 -0700

    Update buzzer.c

--- Project/src/buzzer.c
+++ Project/src/buzzer.c
@@ -2,57 +2,51 @@
 #include "buzzer.h"
 #include "libTimer.h"
 
-// Initializes the buzzer
 void buzzer_init() {
-    timerAUpmode();  // Set up Timer A
-    P2SEL2 &= ~(BIT6 | BIT7);
+    timerAUpmode();        // Initialize Timer A
+    P2SEL2 &= ~(BIT6 | BIT7); // Set buzzer pin function
     P2SEL &= ~BIT7;
     P2SEL |= BIT6;
-    P2DIR |= BIT6;
+    P2DIR |= BIT6;         // Set buzzer pin as output
 }
 
-// Sets the buzzer frequency
 void buzzer_set_period(short cycles) {
-    CCR0 = cycles;           // Set period
-    CCR1 = cycles >> 1;      // Set 50% duty cycle
+    CCR0 = cycles;         // Set buzzer period
+    CCR1 = cycles >> 1;    // 50% duty cycle
 }
 
-// Play Jingle 1
 void play_jingle1() {
-    short jingle1[] = {1000, 0, 800, 0, 600, 0, 400, 0};
+    short notes[] = {1000, 0, 800, 0, 600, 0, 400, 0};
     for (int i = 0; i < 8; i++) {
-        buzzer_set_period(jingle1[i]);
+        buzzer_set_period(notes[i]);
         __delay_cycles(500000);
     }
-    buzzer_set_period(0); // Turn off sound
+    buzzer_set_period(0);  // Turn off buzzer
 }
 
-// Play Jingle 2
 void play_jingle2() {
-    short jingle2[] = {500, 0, 700, 0, 900, 0, 1100, 0};
+    short notes[] = {500, 0, 700, 0, 900, 0, 1100, 0};
     for (int i = 0; i < 8; i++) {
-        buzzer_set_period(jingle2[i]);
+        buzzer_set_period(notes[i]);
         __delay_cycles(500000);
     }
-    buzzer_set_period(0); // Turn off sound
+    buzzer_set_period(0);
 }
 
-// Play Jingle 3
 void play_jingle3() {
-    short jingle3[] = {1200, 0, 1000, 0, 800, 0, 600, 0};
+    short notes[] = {1200, 0, 1000, 0, 800, 0, 600, 0};
     for (int i = 0; i < 8; i++) {
-        buzzer_set_period(jingle3[i]);
+        buzzer_set_period(notes[i]);
         __delay_cycles(500000);
     }
-    buzzer_set_period(0); // Turn off sound
+    buzzer_set_period(0);
 }
 
-// Play Jingle 4
 void play_jingle4() {
-    short jingle4[] = {1500, 0, 1300, 0, 1100, 0, 900, 0};
+    short notes[] = {1500, 0, 1300, 0, 1100, 0, 900, 0};
     for (int i = 0; i < 8; i++) {
-        buzzer_set_period(jingle4[i]);
+        buzzer_set_period(notes[i]);
         __delay_cycles(500000);
     }
-    buzzer_set_period(0); // Turn off sound
+    buzzer_set_period(0);
 }
