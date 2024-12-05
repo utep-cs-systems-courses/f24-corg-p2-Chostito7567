e2c7d9c8156adfcc1728a3c93bd3c5f63ce9c507
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-04 19:46:05 -0700

    Update buzzer.c

--- Project/src/buzzer.c
+++ Project/src/buzzer.c
@@ -1,5 +1,5 @@
 void play_jingle1() {
-    short jingle1[] = {1200, 0, 1000, 0, 800, 0, 600, 0}; // Higher starting pitch
+    short jingle1[] = {1000, 0, 800, 0, 600, 0, 400, 0};
     for (int i = 0; i < 8; i++) {
         buzzer_set_period(jingle1[i]);
         __delay_cycles(500000);
@@ -8,7 +8,7 @@ void play_jingle1() {
 }
 
 void play_jingle2() {
-    short jingle2[] = {900, 0, 1100, 0, 1300, 0, 1500, 0}; // Increasing pitch
+    short jingle2[] = {500, 0, 700, 0, 900, 0, 1100, 0};
     for (int i = 0; i < 8; i++) {
         buzzer_set_period(jingle2[i]);
         __delay_cycles(500000);
@@ -17,7 +17,7 @@ void play_jingle2() {
 }
 
 void play_jingle3() {
-    short jingle3[] = {500, 0, 700, 0, 900, 0, 1100, 0}; // Moderate pitch range
+    short jingle3[] = {1200, 0, 1000, 0, 800, 0, 600, 0};
     for (int i = 0; i < 8; i++) {
         buzzer_set_period(jingle3[i]);
         __delay_cycles(500000);
@@ -26,7 +26,7 @@ void play_jingle3() {
 }
 
 void play_jingle4() {
-    short jingle4[] = {1400, 0, 1200, 0, 1000, 0, 800, 0}; // Descending pitch
+    short jingle4[] = {1500, 0, 1300, 0, 1100, 0, 900, 0};
     for (int i = 0; i < 8; i++) {
         buzzer_set_period(jingle4[i]);
         __delay_cycles(500000);
