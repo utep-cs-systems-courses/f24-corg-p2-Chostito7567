a1786ab2dc27b6f07dcb6b69393aee730502de04
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-04 18:41:35 -0700

    Update led.c

--- Project/src/led.c
+++ Project/src/led.c
@@ -1,25 +1,27 @@
 #include <msp430.h>
 #include "led.h"
 
-unsigned char led_state = 0;   // Current LED state
-unsigned char led_changed = 0; // Flag for changes in LED state
+unsigned char red_on = 0, green_on = 0, led_changed = 0;
 
 void led_init() {
-    P1DIR |= LEDS;  // Set LED pins as outputs
+    P1DIR |= LEDS;    // Set LED pins as output
+    P1OUT &= ~LEDS;   // Initialize LEDs to be off
     led_changed = 1;
     led_update();
 }
 
 void led_update() {
     if (led_changed) {
-        char ledFlags = 0;  // Default: LEDs off
-        if (led_state == 0) {
-            ledFlags |= LED_RED;  // Red LED on
-        } else if (led_state == 1) {
-            ledFlags |= LED_GREEN;  // Green LED on
+        char ledFlags = 0; // Reset LED flags
+
+        if (red_on) {
+            ledFlags |= LED_RED;  // Turn on red LED
         }
-        P1OUT &= ~LEDS;  // Clear LEDs
-        P1OUT |= ledFlags;
+        if (green_on) {
+            ledFlags |= LED_GREEN; // Turn on green LED
+        }
+
+        P1OUT = (P1OUT & ~LEDS) | ledFlags; // Update LEDs
         led_changed = 0;
     }
 }
