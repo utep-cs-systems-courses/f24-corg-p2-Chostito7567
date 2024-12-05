e3fad10d49d837ec423e0f70e90461eceae0eb1f
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-05 10:06:41 -0700

    Update main.c

--- Project/src/main.c
+++ Project/src/main.c
@@ -1,25 +1,25 @@
-
-
 #include <msp430.h>
 #include "led.h"
 #include "stateMachines.h"
 #include "buzzer.h"
 #include "switches.h"
 #include "libTimer.h"
+#include "lcdgame.h"
 
-void main()
-{
+void main() {
     // Timer
     configureClocks();          // Start Lib Timer
 
-    // Initialize 
+    // Initialize peripherals
     switch_init();              // Setup Switches
     buzzer_init();              // Call Speakers
     led_init();                 // Call LEDs
+    lcd_game_init();            // Initialize LCD
 
-    // Watchdog
-    enableWDTInterrupts();      // Enable Watchdog Timer
+    // Draw a square on the LCD
+    lcd_draw_square();
 
     // Power off CPU
+    enableWDTInterrupts();      // Enable Watchdog Timer
     or_sr(0x18);
 }
