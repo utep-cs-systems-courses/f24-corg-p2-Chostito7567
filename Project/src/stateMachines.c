d6d24a538fa091ab2f55b363c8f56499a39f9af5
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-04 18:51:52 -0700

    Update stateMachines.c

--- Project/src/stateMachines.c
+++ Project/src/stateMachines.c
@@ -3,73 +3,33 @@
 #include "led.h"
 #include "switches.h"
 
+// Enum for LED modes
 static enum {off=0, dim=1, bright=2} ledMode;
-static char pwmCount = 0;
+static char pwmCount = 0;  // Counter for PWM duty cycle
 
-void sm_slow_clock()  /* Slowly cycle through {off, dim, bright} */
-{
-  ledMode = (ledMode + 1) % 3;
+// Slowly cycle through LED modes
+void sm_slow_clock() {
+    ledMode = (ledMode + 1) % 3;  // Cycle through off, dim, and bright
 }
 
-void sm_fast_clock()  /* Quickly cycle through 0...3 */
-{
-  pwmCount = (pwmCount + 1) & 3; 
+// Quickly cycle through PWM levels
+void sm_fast_clock() {
+    pwmCount = (pwmCount + 1) & 3;  // Cycle through 0 to 3
 }
 
-void sm_update_led()
-{
-  char new_red_on;
-  switch (ledMode){
-  case off:
-    new_red_on = 0; break;
-  case bright:
-    new_red_on = 1; break;
-  case dim:
-    new_red_on = (pwmCount < 1); break; /* 25% duty cycle */
-  }
-  if (red_on != new_red_on)
-  {
-    red_on = new_red_on;
-    led_changed = 1;
-  }
-}
-
-char toggle_red()  /* Always toggle! */
-{
-  static char state = 0;
-  switch (state) {
-  case 0:
-    red_on = 1;
-    state = 1;
-    break;
-  case 1:
-    red_on = 0;
-    state = 0;
-    break;
-  }
-  return 1;  /* Always changes an LED */
-}
-
-char toggle_green()  /* Only toggle green if red is on! */
-{
-  char changed = 0;
-  if (red_on) {
-    green_on ^= 1;
-    changed = 1;
-  }
-  return changed;
-}
-
-void state_advance()  /* Alternate between toggling red & green */
-{
-  char changed = 0;
-
-  static enum {R=0, G=1} color = G;
-  switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
-  }
-
-  led_changed = changed;  // Switch state changed
-  led_update();
+// Update the LED based on the mode
+void sm_update_led() {
+    char new_red_on;
+    switch (ledMode) {
+    case off:
+        new_red_on = 0; break;       // Red LED off
+    case bright:
+        new_red_on = 1; break;       // Red LED fully on
+    case dim:
+        new_red_on = (pwmCount < 1); break; // 25% duty cycle
+    }
+    if (red_on != new_red_on) {
+        red_on = new_red_on;         // Update LED state
+        led_changed = 1;             // Mark LEDs as changed
+    }
 }
