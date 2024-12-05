d458c93fe17d52b519868b5736f64186c4e46651
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-04 17:44:27 -0700

    Update p2_interrupt_handler.c

--- Project/src/p2_interrupt_handler.c
+++ Project/src/p2_interrupt_handler.c
@@ -1,15 +1,9 @@
-
-
 #include <msp430.h>
 #include "switches.h"
 
-/* Switch on P1 (S2) */
-void
-__interrupt_vec(PORT2_VECTOR) Port_2(){ //P1
-  if (P2IFG & SWITCHES) {	              // P1
-    P2IFG &= ~SWITCHES;		              // P1
-    // Call switches.h
-    switch_interrupt_handler();	
-  }
+void __interrupt_vec(PORT2_VECTOR) Port_2() {
+    if (P2IFG & SWITCHES) {  // Check if a switch caused the interrupt
+        P2IFG &= ~SWITCHES;  // Clear the interrupt flag
+        switch_interrupt_handler();  // Call the switch handler
+    }
 }
-
