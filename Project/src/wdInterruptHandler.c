3674208adf1c9d45fa7cebfc8ccbb3502819a667
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-04 18:35:11 -0700

    Update wdInterruptHandler.c

--- Project/src/wdInterruptHandler.c
+++ Project/src/wdInterruptHandler.c
@@ -1,17 +1,5 @@
-
-
 #include <msp430.h>
-#include "stateMachines.h"
 
-void
-__interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
-  static char blink_count = 0;
-  if (++blink_count == 125) {
-     sm_slow_clock();
-     blink_count = 0;
-  }
-  sm_fast_clock();
-  sm_update_led();
-
-  led_update();
+void __interrupt_vec(WDT_VECTOR) WDT() {
+    // This is for the watchdog timer (optional, can remain empty for now)
 }
