7eb1935940724a0006755db3d43382ecb5ef55a3
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-04 17:57:01 -0700

    Update p2_interrupt_handler.c

--- Project/src/p2_interrupt_handler.c
+++ Project/src/p2_interrupt_handler.c
@@ -1,9 +1,10 @@
 #include <msp430.h>
 #include "switches.h"
 
+// Handles button press interrupts
 void __interrupt_vec(PORT2_VECTOR) Port_2() {
-    if (P2IFG & SWITCHES) {  // Check if a switch caused the interrupt
-        P2IFG &= ~SWITCHES;  // Clear the interrupt flag
-        switch_interrupt_handler();  // Call the switch handler
+    if (P2IFG & SWITCHES) {  // Check if a button caused the interrupt
+        P2IFG &= ~SWITCHES;  // Clear interrupt flags
+        switch_interrupt_handler();  // Handle button press
     }
 }
